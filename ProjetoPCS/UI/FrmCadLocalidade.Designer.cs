1c89d86e2142f47a39473004d70b65c867266e42
jefferson.barnabe@gmail.com <jefferson.barnabe@gmail.com@5fcdac5e-2bcb-11de-a1c6-114bfcb2beae>
2009-04-23 18:36:08 +0000

    Solucionando o problema da navegação.

--- ProjetoPCS/UI/FrmCadLocalidade.Designer.cs
+++ ProjetoPCS/UI/FrmCadLocalidade.Designer.cs
@@ -28,130 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnUltimo = new System.Windows.Forms.Button();
-            this.btnProximo = new System.Windows.Forms.Button();
-            this.btnAnterior = new System.Windows.Forms.Button();
-            this.btnPrimeiro = new System.Windows.Forms.Button();
-            this.btnAlterar = new System.Windows.Forms.Button();
-            this.btnRemover = new System.Windows.Forms.Button();
-            this.btnNovo = new System.Windows.Forms.Button();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnGravar = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCadLocalidade));
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.tlPrincipal = new System.Windows.Forms.ToolStrip();
+            this.btnNovo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnGravar = new System.Windows.Forms.ToolStripButton();
+            this.btnCancelar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnAlterar = new System.Windows.Forms.ToolStripButton();
+            this.btnRemover = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnPrimeiro = new System.Windows.Forms.ToolStripButton();
+            this.btnAnterior = new System.Windows.Forms.ToolStripButton();
+            this.btnProximo = new System.Windows.Forms.ToolStripButton();
+            this.btnUltimo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.txtLocalizar = new System.Windows.Forms.ToolStripTextBox();
+            this.stPrincipal = new System.Windows.Forms.StatusStrip();
+            this.lbInformacao = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bsLocalidade = new System.Windows.Forms.BindingSource(this.components);
+            this.tlPrincipal.SuspendLayout();
+            this.stPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bsLocalidade)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnUltimo
+            // txtNome
             // 
-            this.btnUltimo.Location = new System.Drawing.Point(264, 278);
-            this.btnUltimo.Name = "btnUltimo";
-            this.btnUltimo.Size = new System.Drawing.Size(75, 23);
-            this.btnUltimo.TabIndex = 125;
-            this.btnUltimo.Text = ">>";
-            this.btnUltimo.UseVisualStyleBackColor = true;
+            this.txtNome.Location = new System.Drawing.Point(69, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(221, 20);
+            this.txtNome.TabIndex = 131;
             // 
-            // btnProximo
+            // lbNome
             // 
-            this.btnProximo.Location = new System.Drawing.Point(183, 278);
-            this.btnProximo.Name = "btnProximo";
-            this.btnProximo.Size = new System.Drawing.Size(75, 23);
-            this.btnProximo.TabIndex = 124;
-            this.btnProximo.Text = ">";
-            this.btnProximo.UseVisualStyleBackColor = true;
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(12, 35);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(38, 13);
+            this.lbNome.TabIndex = 132;
+            this.lbNome.Text = "Nome:";
             // 
-            // btnAnterior
+            // tlPrincipal
             // 
-            this.btnAnterior.Location = new System.Drawing.Point(102, 278);
-            this.btnAnterior.Name = "btnAnterior";
-            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
-            this.btnAnterior.TabIndex = 123;
-            this.btnAnterior.Text = "<";
-            this.btnAnterior.UseVisualStyleBackColor = true;
-            // 
-            // btnPrimeiro
-            // 
-            this.btnPrimeiro.Location = new System.Drawing.Point(21, 278);
-            this.btnPrimeiro.Name = "btnPrimeiro";
-            this.btnPrimeiro.Size = new System.Drawing.Size(75, 23);
-            this.btnPrimeiro.TabIndex = 122;
-            this.btnPrimeiro.Text = "<<";
-            this.btnPrimeiro.UseVisualStyleBackColor = true;
-            // 
-            // btnAlterar
-            // 
-            this.btnAlterar.Location = new System.Drawing.Point(321, 127);
-            this.btnAlterar.Name = "btnAlterar";
-            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
-            this.btnAlterar.TabIndex = 130;
-            this.btnAlterar.Text = "Alterar";
-            this.btnAlterar.UseVisualStyleBackColor = true;
-            // 
-            // btnRemover
-            // 
-            this.btnRemover.Location = new System.Drawing.Point(321, 168);
-            this.btnRemover.Name = "btnRemover";
-            this.btnRemover.Size = new System.Drawing.Size(75, 23);
-            this.btnRemover.TabIndex = 129;
-            this.btnRemover.Text = "Remover";
-            this.btnRemover.UseVisualStyleBackColor = true;
+            this.tlPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNovo,
+            this.toolStripSeparator1,
+            this.btnGravar,
+            this.btnCancelar,
+            this.toolStripSeparator2,
+            this.btnAlterar,
+            this.btnRemover,
+            this.toolStripSeparator3,
+            this.btnPrimeiro,
+            this.btnAnterior,
+            this.btnProximo,
+            this.btnUltimo,
+            this.toolStripSeparator4,
+            this.txtLocalizar});
+            this.tlPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tlPrincipal.Name = "tlPrincipal";
+            this.tlPrincipal.Size = new System.Drawing.Size(359, 25);
+            this.tlPrincipal.TabIndex = 133;
+            this.tlPrincipal.Text = "toolStrip1";
             // 
             // btnNovo
             // 
-            this.btnNovo.Location = new System.Drawing.Point(321, 13);
+            this.btnNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNovo.Image = ((System.Drawing.Image)(resources.GetObject("btnNovo.Image")));
+            this.btnNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnNovo.Name = "btnNovo";
-            this.btnNovo.Size = new System.Drawing.Size(75, 23);
-            this.btnNovo.TabIndex = 128;
-            this.btnNovo.Text = "Novo";
-            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Size = new System.Drawing.Size(23, 22);
+            this.btnNovo.ToolTipText = "Clique aqui para incluir uma  nova localidade";
             this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
             // 
-            // btnCancelar
+            // toolStripSeparator1
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(321, 85);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
-            this.btnCancelar.TabIndex = 127;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // btnGravar
             // 
-            this.btnGravar.Location = new System.Drawing.Point(321, 56);
+            this.btnGravar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnGravar.Image = ((System.Drawing.Image)(resources.GetObject("btnGravar.Image")));
+            this.btnGravar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnGravar.Name = "btnGravar";
-            this.btnGravar.Size = new System.Drawing.Size(75, 23);
-            this.btnGravar.TabIndex = 126;
-            this.btnGravar.Text = "Gravar";
-            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Size = new System.Drawing.Size(23, 22);
+            this.btnGravar.ToolTipText = "Clique aqui para confirmar";
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(23, 22);
+            this.btnCancelar.ToolTipText = "Clique aqui para cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAlterar.Image = ((System.Drawing.Image)(resources.GetObject("btnAlterar.Image")));
+            this.btnAlterar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(23, 22);
+            this.btnAlterar.ToolTipText = "Clique aqui para alterar";
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRemover.Image = ((System.Drawing.Image)(resources.GetObject("btnRemover.Image")));
+            this.btnRemover.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(23, 22);
+            this.btnRemover.ToolTipText = "Clique aqui para remover";
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnPrimeiro
+            // 
+            this.btnPrimeiro.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnPrimeiro.Image = ((System.Drawing.Image)(resources.GetObject("btnPrimeiro.Image")));
+            this.btnPrimeiro.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPrimeiro.Name = "btnPrimeiro";
+            this.btnPrimeiro.Size = new System.Drawing.Size(27, 22);
+            this.btnPrimeiro.Text = "<<";
+            this.btnPrimeiro.ToolTipText = "Vai para o primeiro elemento";
+            this.btnPrimeiro.Click += new System.EventHandler(this.btnPrimeiro_Click);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAnterior.Image = ((System.Drawing.Image)(resources.GetObject("btnAnterior.Image")));
+            this.btnAnterior.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(23, 22);
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.ToolTipText = "Vai para o elemento anterior";
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnProximo
+            // 
+            this.btnProximo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnProximo.Image = ((System.Drawing.Image)(resources.GetObject("btnProximo.Image")));
+            this.btnProximo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnProximo.Name = "btnProximo";
+            this.btnProximo.Size = new System.Drawing.Size(23, 22);
+            this.btnProximo.Text = ">";
+            this.btnProximo.ToolTipText = "Vai para o proximo elemento";
+            this.btnProximo.Click += new System.EventHandler(this.btnProximo_Click);
+            // 
+            // btnUltimo
+            // 
+            this.btnUltimo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnUltimo.Image = ((System.Drawing.Image)(resources.GetObject("btnUltimo.Image")));
+            this.btnUltimo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnUltimo.Name = "btnUltimo";
+            this.btnUltimo.Size = new System.Drawing.Size(27, 22);
+            this.btnUltimo.Text = ">>";
+            this.btnUltimo.ToolTipText = "Vai para o último elemento";
+            this.btnUltimo.Click += new System.EventHandler(this.btnUltimo_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // txtLocalizar
+            // 
+            this.txtLocalizar.Name = "txtLocalizar";
+            this.txtLocalizar.Size = new System.Drawing.Size(100, 23);
+            this.txtLocalizar.ToolTipText = "Informe o nome ou parte dele para localiza-lo";
+            // 
+            // stPrincipal
+            // 
+            this.stPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbInformacao});
+            this.stPrincipal.Location = new System.Drawing.Point(0, 184);
+            this.stPrincipal.Name = "stPrincipal";
+            this.stPrincipal.Size = new System.Drawing.Size(359, 22);
+            this.stPrincipal.TabIndex = 134;
+            // 
+            // lbInformacao
+            // 
+            this.lbInformacao.Name = "lbInformacao";
+            this.lbInformacao.Size = new System.Drawing.Size(68, 17);
+            this.lbInformacao.Text = "informação";
             // 
             // FrmCadLocalidade
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 313);
-            this.Controls.Add(this.btnAlterar);
-            this.Controls.Add(this.btnRemover);
-            this.Controls.Add(this.btnNovo);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnGravar);
-            this.Controls.Add(this.btnUltimo);
-            this.Controls.Add(this.btnProximo);
-            this.Controls.Add(this.btnAnterior);
-            this.Controls.Add(this.btnPrimeiro);
+            this.ClientSize = new System.Drawing.Size(359, 206);
+            this.Controls.Add(this.stPrincipal);
+            this.Controls.Add(this.tlPrincipal);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.txtNome);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FrmCadLocalidade";
             this.Text = "FrmCadLocalidade";
+            this.Load += new System.EventHandler(this.FrmCadLocalidade_Load);
+            this.tlPrincipal.ResumeLayout(false);
+            this.tlPrincipal.PerformLayout();
+            this.stPrincipal.ResumeLayout(false);
+            this.stPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bsLocalidade)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnUltimo;
-        private System.Windows.Forms.Button btnProximo;
-        private System.Windows.Forms.Button btnAnterior;
-        private System.Windows.Forms.Button btnPrimeiro;
-        private System.Windows.Forms.Button btnAlterar;
-        private System.Windows.Forms.Button btnRemover;
-        private System.Windows.Forms.Button btnNovo;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.ToolStrip tlPrincipal;
+        private System.Windows.Forms.ToolStripButton btnNovo;
+        private System.Windows.Forms.ToolStripButton btnGravar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnCancelar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnAlterar;
+        private System.Windows.Forms.ToolStripButton btnRemover;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton btnPrimeiro;
+        private System.Windows.Forms.ToolStripButton btnAnterior;
+        private System.Windows.Forms.ToolStripButton btnProximo;
+        private System.Windows.Forms.ToolStripButton btnUltimo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripTextBox txtLocalizar;
+        private System.Windows.Forms.StatusStrip stPrincipal;
+        private System.Windows.Forms.ToolStripStatusLabel lbInformacao;
+        private System.Windows.Forms.BindingSource bsLocalidade;
     }
 }
