86cc30be9b325da548d3e6f742e09153d0039780
jefferson.barnabe@gmail.com <jefferson.barnabe@gmail.com@5fcdac5e-2bcb-11de-a1c6-114bfcb2beae>
2009-04-26 15:41:40 +0000

    Modificando a classe dependente para ter um empregado.

--- trunk/ProjetoPCS/ClassesBasicas/Dependente.cs
+++ trunk/ProjetoPCS/ClassesBasicas/Dependente.cs
@@ -14,14 +14,22 @@ namespace ClassesBasicas
             set { grauParentesco = value; }
         }
 
+        private Empregado empregado;
+        public Empregado Empregado
+        {
+            get { return empregado; }
+            set { empregado = value; }
+        }
+
         public Dependente()
             : base()
         {
         }
-        public Dependente(int codigo, string nome, DateTime dataNascimento, char sexo, string grauParentesco)
+        public Dependente(int codigo, string nome, DateTime dataNascimento, char sexo, string grauParentesco, Empregado empregado)
             : base(codigo, nome, dataNascimento, sexo)
         {
             this.grauParentesco = grauParentesco;
+            this.Empregado = empregado;
         }
     }
 }
